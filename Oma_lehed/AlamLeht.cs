291e35695771d27d65fc8b46fbf69208235768e5
opilane <opilane@HP-CZC2349HT9>
2023-03-17 12:22:09 +0200

    Add project files.

---
+++ Oma_lehed/AlamLeht.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Oma_lehed
+{
+    public class AlamLeht: ContentPage
+    {
+        Label lbl;
+        Image img;
+        Switch sw;
+        public AlamLeht(string title, string file)
+        {
+            lbl = new Label
+            {
+                Text= title,
+                FontAttributes= FontAttributes.Bold,
+                FontSize=Device.GetNamedSize(NamedSize.Title,typeof(Label)),
+                TextColor= Color.YellowGreen,
+                HorizontalOptions=LayoutOptions.Center
+            };
+            sw = new Switch
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                IsToggled = true
+            };
+            sw.Toggled += Sw_Toggled;
+            img = new Image { Source = file };
+            Content= new StackLayout { Children = { lbl,sw,img } };
+        }
+
+        private void Sw_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (e.Value== true)
+            {
+                img.IsVisible = true;
+            }
+            else
+            {
+                img.IsVisible = false;
+            }
+        }
+    }
+}
---
+++ Oma_lehed/Start.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Oma_lehed
+{
+    public class Start:ContentPage
+    {
+        public List<Button> buttons { get; set; }
+        List<ContentPage> pages { get; set; }
+        Picker pk;
+        public Start()
+        {
+            StackLayout st = new StackLayout();
+            buttons= new List<Button>();
+            pages= new List<ContentPage>();
+            List<string> files = new List<string> { "green.jpg", "red.jpg", "yellow.jpg" };
+            List<string> dirs = new List<string>() { "green", "red", "yellow" };
+            Random rnd = new Random();
+            for (int i = 0; i < files.Count; i++)
+            {
+                Button b = new Button
+                {
+                    Text = dirs[i],
+                    TabIndex = i
+                };
+                buttons.Add(b);
+                st.Children.Add(b);
+                b.Clicked += B_Clicked;
+
+                AlamLeht p = new AlamLeht(dirs[i], files[i]);
+                pages.Add(p);
+            }
+            pk = new Picker
+            {
+                ItemsSource = dirs,
+                Title = "Tee valik",
+                TitleColor = Color.YellowGreen
+            };
+            pk.SelectedIndexChanged += Pk_SelectedIndexChanged;
+            st.Children.Add(pk);
+            Content = st;
+        }
+
+        private async void Pk_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(pages[pk.SelectedIndex]);
+        }
+
+        private async void B_Clicked(object sender, EventArgs e)
+        {
+            Button b=sender as Button;
+            await Navigation.PushAsync(pages[b.TabIndex]);
+        }
+    }
+}
